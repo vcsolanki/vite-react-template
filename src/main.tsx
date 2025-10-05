de77fc6e93b7890fe4c111a8b97013c0b4700135
Vishal Solanki <vcsolanki.vs@gmail.com>
2025-10-05 13:03:58 +0530

    Refactor main.tsx for consistent formatting and style

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import './main.css'
-import ReactDOM from 'react-dom/client'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { StrictMode } from "react";
+import "./main.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import ReactDOM from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
-if(rootElement === null) throw new Error("Failed to find the root element");
+if (rootElement === null) throw new Error("Failed to find the root element");
 
-const route = createRouter({ routeTree })
+const route = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
-    interface Register {
-        router: typeof route
-    }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof route;
+	}
 }
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <StrictMode>
-        <RouterProvider router={route} />
-    </StrictMode>,
-)
+	<StrictMode>
+		<RouterProvider router={route} />
+	</StrictMode>,
+);
